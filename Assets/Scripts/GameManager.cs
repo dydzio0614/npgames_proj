25217b3f71261b8bb7dd63b32e02e5e3a199a2fc
Dydzio <blood990@gmail.com>
2023-09-24 21:45:49 +0200

    Add logic for spawning objects based on grid

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -31,13 +30,37 @@ public class GameManager : MonoBehaviour
     {
         currentObjectsQuantity = simulationObjectsQuantity;
         
+        List<Vector2> possibleSpawnPoints = GenerateSpawnPositionsList();
+        System.Random random = new System.Random();
+        
         for (int i = 0; i < simulationObjectsQuantity; i++)
         {
+            int spawnSpotIndex = random.Next(possibleSpawnPoints.Count);
+            Vector2 spawnSpot = possibleSpawnPoints[spawnSpotIndex];
+            possibleSpawnPoints.RemoveAt(spawnSpotIndex);
+            
             var spawnedObject = Instantiate(simulationObjectPrefab,
-                new Vector3(UnityEngine.Random.Range(-55f, 55f), 0.5f, UnityEngine.Random.Range(-30f, 30f)), Quaternion.identity, simulationObjectsContainer);
-
+                new Vector3(UnityEngine.Random.Range(spawnSpot.x - 0.25f, spawnSpot.x + 0.25f), 0.5f, UnityEngine.Random.Range(spawnSpot.y - 0.25f, spawnSpot.y + 0.25f)), Quaternion.identity, simulationObjectsContainer);
             spawnedObject.GetComponent<SimulationObjectController>().OnDeath += SimulationObjectDeathEventHandler;
         }
+    }
+
+    private List<Vector2> GenerateSpawnPositionsList()
+    {
+        List<Vector2> result = new List<Vector2>(); 
+        
+        //assumptions: starting positions and spots density based on current viewport, safe position size based on 1f width/length object size
+        const float xStart = -55f;
+        const float yStart = -30f;
+        const float dimensionSize = 2f;
+        const int numberOfRows = 30;
+        const int numberOfColumns = 55;
+
+        for (int i = 0; i < numberOfRows; i++)
+            for (int j = 0; j < numberOfColumns; j++)
+                result.Add(new Vector2(xStart + j * dimensionSize, yStart + i * dimensionSize));
+
+        return result;
     }
 
     private void SimulationObjectDeathEventHandler()
